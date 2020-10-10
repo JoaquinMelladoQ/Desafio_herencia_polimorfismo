12f0de9d846869d1145ed14cdabf5987e21e42df
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-10 18:33:01 -0300

    defining inheritance from class

--- animal.rb
+++ animal.rb
@@ -44,3 +44,39 @@ class Animal
     end
 
 end
+
+class Ave < Animal
+end
+
+class Mamifero < Animal
+end
+
+class Insecto < Animal
+end
+
+class Pinguino < Ave
+end
+
+class Paloma < Ave
+end
+
+class Pato < Ave
+end
+
+class Perro < Mamifero
+end
+
+class Gato < Mamifero
+end
+
+class Vaca < Mamifero
+end
+
+class Mosca < Insecto
+end
+
+class Mariposa < Insecto
+end
+
+class Abeja < Insecto
+end
