9f175155ff407b96219dbc6254bb46b223f0a8b3
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-10 19:09:46 -0300

    Adding content to classes

--- person.rb
+++ person.rb
@@ -9,17 +9,43 @@ class Person
     end
 
     def birthday
-        
+        @age += 1
     end
-
 
 end
 
 class Student < Person
+
+    def talk
+        puts "Aquí es la clase de programación con Ruby?"
+    end
+
+    def introduce
+        puts ""
+    end
+
 end
 
 class Teacher < Person
+
+    def talk
+        puts ""
+    end
+
+    def introduce
+        puts ""
+    end
+
 end
 
 class Parent < Person
+
+    def talk
+        puts ""
+    end
+
+    def introduce
+        puts ""
+    end
+
 end
