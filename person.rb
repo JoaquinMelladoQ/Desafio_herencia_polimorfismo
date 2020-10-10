3f7410d712ee19f8ded97d7a1a5ad35af7a67514
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-10-10 19:05:24 -0300

    Added constructor to class person

--- person.rb
+++ person.rb
@@ -1,11 +1,25 @@
 class Person
+
+    attr_reader :first_name, :last_name, :age
+
+    def initialize( first, last, age )
+        @first_name = first
+        @last_name = last
+        @age = age
+    end
+
+    def birthday
+        
+    end
+
+
 end
 
-class Student
+class Student < Person
 end
 
-class Teacher
+class Teacher < Person
 end
 
-class Parent
+class Parent < Person
 end
